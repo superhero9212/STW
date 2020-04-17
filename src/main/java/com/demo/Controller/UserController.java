cf991cf1472a33b0a63a57d298d2739a2e203b2a
liweixiang <liweixiang@stw.com>
2020-04-17 09:22:57 +0800

    test

---
+++ src/main/java/com/demo/Controller/UserController.java
@@ -0,0 +1,10 @@
+package com.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+    public static void main(String[] args) {
+        System.out.println("UserController");
+    }
+}
