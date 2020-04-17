cfaadd1e99a2c42b6d531fc9a1ea23ccc4489a36
HuZ <hjzhjzmy@gmail.com>
2020-04-17 09:46:30 +0800

    test

--- src/main/java/com/demo/Controller/UserController.java
+++ src/main/java/com/demo/Controller/UserController.java
@@ -6,5 +6,9 @@ import org.springframework.stereotype.Controller;
 public class UserController {
     public static void main(String[] args) {
         System.out.println("UserController");
+        String a = "abc";
+        String b = "abc";
+        System.out.println(a=b);
+        System.out.println(a.equals(b));
     }
 }
